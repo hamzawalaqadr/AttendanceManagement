dd0edaccc6e18a4880a00e7a44bd9f48f108a704
muzairamin <muzair.amin@systemsltd.com>
2023-08-03 17:07:40 +0500

    intial

---
+++ src/test/java/com/example/AttendanceMangement/AttendanceManagement/AttendanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AttendanceMangement.AttendanceManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
