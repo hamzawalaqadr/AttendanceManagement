8d83f47fb41cb9378527f1b2823e9e469aadce19
mtalha.jawed <talha.j771@gmail.com>
2023-08-07 00:35:59 -0700

    Created Packages

--- src/main/java/com/example/AttendanceMangement/AttendanceManagement/AttendanceManagementApplication.java
+++ src/main/java/com/example/AttendanceMangement/AttendanceManagement/AttendanceManagementApplication.java
@@ -1,4 +1,4 @@
-package com.example.AttendanceMangement.AttendanceManagement;
+	package com.example.AttendanceMangement.AttendanceManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
